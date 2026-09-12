9e4f96f650647baf9af960a442c2b8fe4260b920
tunshi <tunshisky@outlook.com>
2026-09-13 00:59:03 +0800

    Initial NestJS project

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const SERVICE_NAME = process.env.SERVICE_NAME ?? 'app';
+const HOST = process.env.HOST ?? 'localhost';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  new Logger(SERVICE_NAME).log(`[${SERVICE_NAME}] http://${HOST}:${port}`);
+}
+bootstrap();
